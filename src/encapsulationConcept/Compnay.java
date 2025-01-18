fdd0a5a1bb4178ebdf63815f4a597d5809b20adf
Akshay Kudale <akshaykudale3@gmail.com>
2025-01-18 08:49:30 +0530

    java sessions

---
+++ src/encapsulationConcept/Compnay.java
@@ -0,0 +1,16 @@
+package encapsulationConcept;
+
+public class Compnay {
+	private Compnay() {
+
+	}
+
+	private static void getRevenue() {
+		System.out.println("get rev");
+	}
+
+	public static void getDetails() {
+		getRevenue();
+	}
+
+}
